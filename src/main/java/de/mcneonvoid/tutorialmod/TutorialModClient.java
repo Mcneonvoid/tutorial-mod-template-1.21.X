2b04682e35093cc7d8a6a317ff913184f21bcdab
Simon <Simon.d.schulte@gmail.com>
2025-04-27 16:51:06 +0200

    Initial commit

---
+++ src/main/java/de/mcneonvoid/tutorialmod/TutorialModClient.java
@@ -0,0 +1,8 @@
+package de.mcneonvoid.tutorialmod;
+import net.fabricmc.api.ClientModInitializer;
+public class TutorialModClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
